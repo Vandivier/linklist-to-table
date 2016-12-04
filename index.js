6fe88532fca0472285c85578f946e4df48d54c72
Vandivier <vandivier_john@yahoo.com>
2016-12-04 18:00:18 -0500

    modularized function;

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 let fs = require('fs');
 let axios = require('axios');
+let l2t = require('./lib/l2t.js');                             //ref: https://nodejs.org/api/modules.html
+
 let sUrl = 'http://www.google.com';
-let sInputFile = __dirname + '\links.txt';
+let sInputFile = __dirname + '\\links.txt';
+let wsOut = fs.createWriteStream('./output.csv');       //todo: allow out as SQL. ref: https://docs.nodejitsu.com/articles/advanced/streams/how-to-use-fs-create-write-stream/
 
 console.log(sInputFile);
-axios.get(sUrl)
 
-.then(function (oResponse) {
-    console.log(oResponse.data);   //todo: parse out the <title></title>
-});
+l2t.handleOneLink(sUrl);
+
+console.log('done.');
