1ace7aff18e825b35ff6dabf0e2bf8526f377000
Vandivier <vandivier_john@yahoo.com>
2016-12-04 19:10:29 -0500

    stable version;

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 'use strict';
 
 let fs = require('fs');
-let axios = require('axios');
+var axios = require('axios');
 let l2t = require('./lib/l2t.js');                             //ref: https://nodejs.org/api/modules.html
 
-let sUrl = 'http://www.google.com';
 let sInputFile = __dirname + '\\links.txt';
-let wsOut = fs.createWriteStream('./output.csv');       //todo: allow out as SQL. ref: https://docs.nodejitsu.com/articles/advanced/streams/how-to-use-fs-create-write-stream/
+let wsOut = fs.createWriteStream('./output.csv');              //todo: allow out as SQL. ref: https://docs.nodejitsu.com/articles/advanced/streams/how-to-use-fs-create-write-stream/
+let rsInStream = fs.createReadStream(sInputFile)
 
 console.log(sInputFile);
-
-l2t.handleOneLink(sUrl);
-
-console.log('done.');
+l2t.linkStreamFromList(rsInStream, wsOut);
+console.log('Done.');
